929a4637a2132bfa46929aa0dc644922f7f11ec3
samkits <samkitsalot0@gmail.com>
2025-04-23 17:15:40 +0530

    Added Special Cell Interface logic.

---
+++ src/SpecialCell.java
@@ -0,0 +1,5 @@
+public interface SpecialCell {
+    public boolean isCellSpecial(int cellNumber);
+    public int cellEffect(int cellNumber);
+    public void addCellToList(int startPoint, int endPoint);
+}
